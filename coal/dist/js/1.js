3dcc41f13cc3175451fa0fc463d90d6889ddf8b3
feng0725 <674965617@qq.com>
2018-01-15 16:01:20 +0800

    生产分析页面更新

--- coal/dist/js/1.js
+++ coal/dist/js/1.js
@@ -4,20 +4,18 @@ var t01 = echarts.init(document.getElementById('t01'), 'customed');
 var t02 = echarts.init(document.getElementById('t02'), 'customed');
 var t03 = echarts.init(document.getElementById('t03'), 'customed');
 var t04 = echarts.init(document.getElementById('t04'), 'customed');
-var t05 = echarts.init(document.getElementById('t05'), 'customed');
 
 setT01({ obj: t01 });
 setT02({ obj: t02 });
 setT03({ obj: t03 });
 setT04({ obj: t04 });
-setT05({ obj: t05 });
 
 function setT01(json) {
 
     var option = {
-        color: ['#aec253', '#0084f1', '#d9a82b'],
+        color: ['#3a8e3a', '#d9a82b', '#d9a82b'],
         title: {
-            text: '销量收入'
+            text: '洗煤生产分析'
         },
         tooltip: {
             trigger: 'axis',
@@ -32,12 +30,12 @@ function setT01(json) {
             containLabel: true
         },
         legend: {
-            data: ['销量收入', '实际', '均线']
+            data: ['洗煤产量', '洗煤回收率', '均线']
 
         },
         xAxis: [{
             type: 'category',
-            data: Feng.monthArr(),
+            data: ['洗大块产量', '洗中块产量', '籽煤产量', '洗沫煤产量', '洗矸产量'],
             axisTick: {
                 alignWithLabel: true
             }
@@ -47,9 +45,14 @@ function setT01(json) {
             name: '单位：万元'
         }],
         series: [{
-            name: '销量收入',
+            name: '洗煤产量',
             type: 'bar',
-            data: Feng.rndArr(12, 10, 100)
+            barWidth: '40%',
+            data: Feng.rndArr(12, 50, 100)
+        }, {
+            name: '洗煤回收率',
+            type: 'line',
+            data: Feng.rndArr(5, 10, 50)
         }]
     };
     json.obj.setOption(option);
@@ -59,7 +62,7 @@ function setT02(json) {
 
     var option = {
         title: {
-            text: '销量利润'
+            text: '原煤生产作业'
         },
         color: ['#d4555e', '#0084f1', '#d9a82b'],
         tooltip: {
@@ -75,12 +78,12 @@ function setT02(json) {
             containLabel: true
         },
         legend: {
-            data: ['销量利润']
+            data: ['掘进进尺', '开拓进尺']
 
         },
         xAxis: [{
             type: 'category',
-            data: Feng.monthArr(),
+            data: ['A公司', 'B公司', 'C公司', 'D公司', 'E公司', 'F公司', 'G公司'],
             axisTick: {
                 alignWithLabel: true
             }
@@ -88,10 +91,14 @@ function setT02(json) {
         yAxis: [{
             type: 'value',
             name: '单位：万元'
-
         }],
         series: [{
-            name: '销量利润',
+            name: '掘进进尺',
+            type: 'line',
+            smooth: true,
+            data: Feng.rndArr(12, 1, 100)
+        }, {
+            name: '开拓进尺',
             type: 'line',
             smooth: true,
             data: Feng.rndArr(12, 1, 100)
@@ -103,9 +110,9 @@ function setT02(json) {
 function setT03(json) {
     var option = {
         title: {
-            text: '近五年销量总之对比分析'
+            text: '产量分析'
         },
-        color: ['#2ec8ca'],
+        color: ['#aec253'],
         tooltip: {
             trigger: 'axis',
             axisPointer: { // 坐标轴指示器，坐标轴触发有效
@@ -119,12 +126,12 @@ function setT03(json) {
             containLabel: true
         },
         legend: {
-            data: ['销量总值']
+            data: ['产量']
 
         },
         xAxis: [{
             type: 'category',
-            data: ['2014年', '2015年', '2016年', '2017年', '2018年'],
+            data: ['原煤产量', '洗煤', '商品煤产量'],
             axisTick: {
                 alignWithLabel: true
             }
@@ -135,9 +142,10 @@ function setT03(json) {
 
         }],
         series: [{
-            name: '销量总值',
+            name: '产量',
             type: 'bar',
-            data: Feng.rndArr(5, 1, 100)
+            barWidth: '40%',
+            data: Feng.rndArr(3, 1, 100)
         }]
     };
     json.obj.setOption(option);
@@ -146,105 +154,31 @@ function setT03(json) {
 function setT04(json) {
     var option = {
         title: {
-            text: '实物量销售对比分析'
+            text: '产量超欠'
         },
-        color: ['#3a8e3a', '#0084f1', '#d9a82b', '#db502c'],
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: { // 坐标轴指示器，坐标轴触发有效
-                type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-            }
-        },
-        grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-        },
+        color: ['#d4555e', '#fddd4d', '#0084f1', '#d9a82b', '#a6d1ff', '#d4555e', '#3a8e3a'],
         legend: {
-            data: ['计划', '实际', '均线', '发热量']
-
+            orient: 'vertical',
+            align: 'left',
+            left: '65%',
+            top: 'center',
+            data: ['原煤', '洗煤', '商品煤']
         },
-        yAxis: [{
-
-            type: 'value',
-            axisTick: {
-                alignWithLabel: true
-            }
-        }],
-        xAxis: [{
-            type: 'category',
-            data: Feng.monthArr()
-        }],
         series: [{
-            name: '计划',
-            type: 'bar',
-            data: Feng.rndArr(12, 1, 100)
-        }, {
-            name: '实际',
-            type: 'bar',
-            data: Feng.rndArr(12, 1, 100)
-        }, {
-            name: '均线',
-            type: 'line',
-            smooth: true,
-            data: Feng.rndArr(12, 1, 50)
-        }, {
-            name: '发热量',
-            type: 'line',
-            smooth: true,
-            data: Feng.rndArr(12, 1, 100)
+            name: '访问来源',
+            type: 'pie',
+            radius: ['0%', '60%'],
+            center: ['40%', '55%'],
+            label: {
+                normal: {
+                    show: false
+                }
+            },
+            data: [{ value: 100, name: '原煤' }, { value: 400, name: '洗煤' }, { value: 350, name: '商品煤' }],
+            roseType: 'radius'
+
         }]
     };
+
     json.obj.setOption(option);
 }
-
-function setT05(json) {
-
-    var option = {
-        title: {
-            text: '各省销售对比分析'
-        },
-        color: ['#9c43a5', '#0084f1', '#d9a82b'],
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: { // 坐标轴指示器，坐标轴触发有效
-                type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-            }
-        },
-        grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-        },
-        legend: {
-            data: ['销售值']
-
-        },
-        xAxis: [{
-            type: 'category',
-            data: ['河南', '新疆', '青海', '贵州', '内蒙古', '山西', '四川', '其他'],
-            axisTick: {
-                alignWithLabel: true
-            }
-        }],
-        yAxis: [{
-            type: 'value',
-            name: '单位：万元'
-
-        }],
-        series: [{
-            name: '销售值',
-            type: 'line',
-            // smooth:true,
-            data: Feng.rndArr(8, 1, 100)
-        }]
-    };
-    json.obj.setOption(option);
-}
-
-$('.t01 ul li ').on('click', function () {
-    $(this).addClass('active').siblings().removeClass('active');
-    setT04({ obj: t04 });
-});
