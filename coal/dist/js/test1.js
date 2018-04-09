70e64a95b76577273de25c6d8842288ccb68d092
wolf <shilei0228@sina.com>
2018-04-09 15:11:30 +0800

    update:更新gulp内容.增加stylus编译,错误整合.浏览器热更新

---
+++ coal/dist/js/test1.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var log = function log(message) {
+	console.log(message);
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,57 @@
-var gulp = require("gulp"),
-    babel = require("gulp-babel"),
-    stylus = require('stylus'),
-    watch = require("gulp-watch");
+var gulp = require("gulp");
+// babel
+var babel = require("gulp-babel");
+// stylus
+var stylus = require('gulp-stylus');
+//  添加浏览器后缀
+var autoprefixer = require('gulp-autoprefixer')
+var watch = require("gulp-watch");
+// 自动同步浏览器插件
+var browserSync = require('browser-sync');
+// 整合 streams 来处理错误
+var combiner = require('stream-combiner2');
 
+// bable 编译
 gulp.task("babeljs", function () {
-    return gulp.src("coal/js/*.js")
-        .pipe(babel())
-        .on('error', function(err) {
-            console.log('babel Error!', err.message);
-            this.end();
-        })
-        .pipe(gulp.dest("coal/dist/js"));
+	var combined = combiner.obj([
+		gulp.src("coal/js/*.js"),
+		babel(),
+		gulp.dest("coal/dist/js"),
+		browserSync.reload({stream: true})])
+
+	combined.on('error', console.error.bind(console))
+	return combined
 });
-gulp.task("watch",function(){
-    gulp.watch('coal/js/*.js',['babeljs']);
+
+// stylus 编译
+gulp.task('stylus', function () {
+	var combined = combiner.obj([
+		gulp.src('coal/css/*.styl'),
+		stylus(),
+		autoprefixer(),
+		gulp.dest('coal/dist/css'),
+		browserSync.reload({stream: true})])
+
+	combined.on('error', console.error.bind(console))
+	return combined
+});
+
+// 监听
+gulp.task("watch", function () {
+	gulp.watch('coal/js/*.js', ['babeljs']);
+	gulp.watch('coal/css/*.styl', ['stylus']);
+	gulp.watch('coal/*.html', browserSync.reload);
 })
 
-// gulp.task('stylus', function () {
-//     return gulp.src('coal/css/*.styl')
-//         .pipe(stylus())
-//         .pipe(gulp.dest('coal/dist/css'));
-// });
-// gulp.task('saswatch', function () {
-//     gulp.watch('coal/css/*.styl', ['stylus']);
-// });
 
-gulp.task('default',['watch','babeljs']);
+
+// 自动更新浏览器任务
+gulp.task('browserSync', function () {
+	browserSync.init({
+		server: {
+			baseDir: 'coal'
+		}
+	})
+});
+gulp.task('compile', ['watch', 'babeljs', 'stylus']);
+gulp.task('default', ['watch', 'babeljs', 'stylus','browserSync']);
